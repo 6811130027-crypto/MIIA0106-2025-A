db53972bdde5037e27833bdae8e4115fa5e6146f
6811130027-crypto <6811130027@mut.ac.th>
2025-11-25 17:06:58 +0700

    การทดลองครั้งที่2

---
+++ TEST02/TEST02.cpp
@@ -0,0 +1,18 @@
+// TEST02.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello LAB2!\n";
+    
+    cout << "-Neme: Woraphon Phonkan" << endl;
+    cout << "-Coures: MIIA0106" << endl;
+    cout << "-Email: 6811130027@mut.ac.th" << endl;
+    return 0;
+
+
+}
+
