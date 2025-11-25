aa21039c5c095bb3084c03a7d3151d024e9bbddb
6811130027-crypto <6811130027@mut.ac.th>
2025-11-25 18:24:33 +0700

    Udate Code ทดลองที่2

---
+++ TESTครั้งที่2.1/TESTครั้งที่2.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip> // เพิ่มเพื่อใช้ในการจัดรูปแบบทศนิยม
+using namespace std;
+
+int main() {
+    string itemName;
+    float price;
+    int qty;
+    float total;
+
+    // รับชื่อสินค้า
+    cout << "Enter item name: ";
+    getline(cin, itemName);
+
+    // รับราคา
+    cout << "Enter price: ";
+    cin >> price;
+
+    // รับจำนวน
+    cout << "Enter quantity: ";
+    cin >> qty;
+
+    // คำนวณราคารวม
+    total = price * qty;
+
+    // แสดงในแสจอ
+    cout << endl; // เว้นบรรทัด
+    cout << "---------- Receipt ----------" << endl;
+    cout << "Item Name: " << itemName << endl;
+
+    // จัดรูปแบบให้แสดงทศนิยม 2 ตำแหน่ง
+    cout << fixed << setprecision(2);
+
+    cout << "Price: " << price << endl;
+    cout << "Quantity: " << qty << endl;
+    cout << "Total: " << total << endl;
+    cout << "-----------------------------" << endl;
+
+    return 0;
+}
---
+++ TESTครั้งที่2.2/TESTครั้งที่2.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string name = "Woraphon Phonkan"; // เปลี่ยน Name
+    int age = 19;                       // เปลี่ยน Age
+    string studentID = "6811130027";    // เปลี่ยน Student ID
+    float height = 169.0;               // เพิ่ม Height
+
+    // แสดงผลลัพธ์ออกหน้าจอ
+    cout << "================ Student Info ================" << endl;
+    cout << "Name      : " << name << endl;
+    cout << "Age       : " << age << " years old" << endl;
+    cout << "Student ID: " << studentID << endl;
+    cout << "Height    : " << height << " cm" << endl; // แสดง Height
+    cout << "==============================================" << endl;
+
+    return 0;
+}
